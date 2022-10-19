912a11a80217cb5c23736fd8b499b338693bb339
rubencho909 <rubencho221@hotmail.com>
2022-10-19 00:34:06 -0500

    Creación de la estructura del proyecto y la clase model

---
+++ src/main/java/com/crud/ecommerce/model/Usuario.java
@@ -0,0 +1,86 @@
+package com.crud.ecommerce.model;
+
+public class Usuario {
+
+	private Integer id;
+	private String nombre;
+	private String userName;
+	private String email;
+	private String direccion;
+	private String telefono;
+	private String tipo;
+	private String password;
+	
+	public Usuario() {
+	}
+	
+	public Usuario(Integer id, String nombre, String userName, String email, String direccion, String telefono,
+			String tipo, String password) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.userName = userName;
+		this.email = email;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.tipo = tipo;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", userName=" + userName + ", email=" + email
+				+ ", direccion=" + direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password
+				+ "]";
+	}
+	
+}
