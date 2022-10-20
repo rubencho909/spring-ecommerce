de972ed2bd743e36dc956eca2495e93c2f9e16c0
rubencho909 <rubencho221@hotmail.com>
2022-10-19 23:56:24 -0500

    Funcionalidad mostrar productos para el administrador

--- src/main/java/com/crud/ecommerce/service/ProductoServiceI.java
+++ src/main/java/com/crud/ecommerce/service/ProductoServiceI.java
@@ -1,11 +1,13 @@
 package com.crud.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.crud.ecommerce.model.Producto;
 
 public interface ProductoServiceI {
 	
+	public List<Producto> findAll();
 	public Producto save(Producto producto);
 	public Optional<Producto> getById(Integer id);
 	public void update(Producto producto);
--- src/main/java/com/crud/ecommerce/service/ProductoServiceImp.java
+++ src/main/java/com/crud/ecommerce/service/ProductoServiceImp.java
@@ -1,5 +1,6 @@
 package com.crud.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,11 @@ public class ProductoServiceImp implements ProductoServiceI{
 	@Override
 	public void delete(Integer id) {
 		productoRepository.deleteById(id);
+	}
+
+	@Override
+	public List<Producto> findAll() {
+		return productoRepository.findAll();
 	}
 
 }
