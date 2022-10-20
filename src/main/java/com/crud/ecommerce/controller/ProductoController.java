a59ae8cb17710f3c25abdee561abe6605f80a1e8
rubencho909 <rubencho221@hotmail.com>
2022-10-20 00:27:42 -0500

    Funcionalidad para editar productos

--- src/main/java/com/crud/ecommerce/controller/ProductoController.java
+++ src/main/java/com/crud/ecommerce/controller/ProductoController.java
@@ -1,10 +1,13 @@
 package com.crud.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,29 +18,45 @@ import com.crud.ecommerce.service.ProductoServiceI;
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
-	
+
 	@Autowired
 	ProductoServiceI productoServiceI;
-	
+
 	@GetMapping("")
 	public String show(Model model) {
 		model.addAttribute("productos", productoServiceI.findAll());
 		return "productos/show";
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
 	}
-	
+
 	@PostMapping("/save")
 	public String save(Producto producto) {
 		LOGGER.info("Este es el objeto producto {}", producto);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "", null, null);
 		producto.setUsuario(u);
 		productoServiceI.save(producto);
+		return "redirect:/productos";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoServiceI.getById(id);
+		producto = optionalProducto.get();
+		LOGGER.info("Producto buscado: {}", producto);
+		model.addAttribute("producto", producto);
+		return "productos/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoServiceI.update(producto);
 		return "redirect:/productos";
 	}
 
