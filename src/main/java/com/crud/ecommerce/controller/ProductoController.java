0c32adc15385262f6fac0a2c54f720a9446a9421
rubencho909 <rubencho221@hotmail.com>
2022-10-19 23:38:16 -0500

    Funcionalidad crear producto, vistra create y Logger para la clase ProductoController

--- src/main/java/com/crud/ecommerce/controller/ProductoController.java
+++ src/main/java/com/crud/ecommerce/controller/ProductoController.java
@@ -1,15 +1,21 @@
 package com.crud.ecommerce.controller;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.crud.ecommerce.model.Producto;
+import com.crud.ecommerce.model.Usuario;
 import com.crud.ecommerce.service.ProductoServiceI;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
 	
 	@Autowired
 	ProductoServiceI productoServiceI;
@@ -17,6 +23,20 @@ public class ProductoController {
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
+	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto) {
+		LOGGER.info("Este es el objeto producto {}", producto);
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "", null, null);
+		producto.setUsuario(u);
+		productoServiceI.save(producto);
+		return "redirect:/productos";
 	}
 
 }
