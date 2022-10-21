1c3a03f22040823d541776b684833ee0b9dc82ca
rubencho909 <rubencho221@hotmail.com>
2022-10-21 08:19:25 -0500

    Redireccion al carrito de compras

--- src/main/java/com/crud/ecommerce/controller/HomeController.java
+++ src/main/java/com/crud/ecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.crud.ecommerce.model.Producto;
@@ -37,6 +38,11 @@ public class HomeController {
 		producto = productOptional.get();
 		model.addAttribute("producto", producto);
 		return "usuario/productoHome";
+	}
+	
+	@PostMapping("/cart")
+	public String addCart() {
+		return "usuario/carrito";
 	}
 	
 }
