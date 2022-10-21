85fbe573ef7df35739e8665dc52c494d39a9be15
rubencho909 <rubencho221@hotmail.com>
2022-10-21 08:05:36 -0500

    mostrar informacion del producto en la vista productohome

--- src/main/java/com/crud/ecommerce/controller/HomeController.java
+++ src/main/java/com/crud/ecommerce/controller/HomeController.java
@@ -1,16 +1,24 @@
 package com.crud.ecommerce.controller;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.crud.ecommerce.model.Producto;
 import com.crud.ecommerce.service.ProductoServiceI;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoServiceI productoServiceI;
@@ -19,6 +27,16 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("productos", productoServiceI.findAll());
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id del Producto enviado como parametro {}", id); 
+		Producto producto = new Producto();
+		Optional<Producto> productOptional = productoServiceI.getById(id);
+		producto = productOptional.get();
+		model.addAttribute("producto", producto);
+		return "usuario/productoHome";
 	}
 	
 }
